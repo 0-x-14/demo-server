e3062ffa98b0daa8f7508aa4cd30b4ce08ae27d3
0-x-14 <0x14@daum.net>
2024-04-26 18:03:37 +0900

    [Fix] 서버명 및 프로젝트명 변경

--- src/main/java/ddareunging/ddareunging_server/DdaeungingServerApplication.java
+++ src/main/java/ddareunging/ddareunging_server/DdaeungingServerApplication.java
@@ -1,13 +1,13 @@
-package demo.demo_server;
+package ddareunging.ddareunging_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoServerApplication {
+public class DdaeungingServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoServerApplication.class, args);
+		SpringApplication.run(DdaeungingServerApplication.class, args);
 	}
 
 }
