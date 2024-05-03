cf0008a14904f7206682c44f79e8ec6a934ce8cd
0-x-14 <0x14@daum.net>
2024-05-04 00:52:52 +0900

    [Feat] 날씨 조회 API 구현

--- src/main/java/ddareunging/ddareunging_server/domain/Weather.java
+++ src/main/java/ddareunging/ddareunging_server/domain/Weather.java
@@ -1,5 +1,15 @@
 package ddareunging.ddareunging_server.domain;
 
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Embeddable
 public class Weather {
     private Double temp; // 기온
     private Double rainAmount; // 강수량
