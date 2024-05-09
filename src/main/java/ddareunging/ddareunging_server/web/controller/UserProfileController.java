a9bcc48e2d5fbf09148809c53d27929163143f7d
0-x-14 <0x14@daum.net>
2024-05-09 14:30:11 +0900

    [Fix] User 관련 경로 수정

--- src/main/java/ddareunging/ddareunging_server/web/controller/UserProfileController.java
+++ src/main/java/ddareunging/ddareunging_server/web/controller/UserProfileController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
@@ -22,13 +21,14 @@ public class UserProfileController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/updateProfile")
+    // @PostMapping("/updateProfile")
+    @PostMapping("/signup")
     public String updateProfile(@RequestParam("nickname") String nickname,
                                 HttpSession session, RedirectAttributes redirectAttributes) {
         User user = (User) session.getAttribute("user");
         if (user == null) {
             redirectAttributes.addFlashAttribute("error", "Session expired. Please log in again.");
-            return "redirect:/login";
+            return "redirect:";
         }
 
         try {
@@ -47,10 +47,10 @@ public class UserProfileController {
     public String showUserInfo(Model model, HttpSession session) {
         User user = (User) session.getAttribute("user");
         if (user == null) {
-            return "redirect:/login";
+            return "redirect:";
         }
         model.addAttribute("user", user);
-        return "userInfo";  // userInfo.html로 리다이렉트
+        return "userinfo";  // userInfo.html로 리다이렉트
     }
 
 }
