88250afe6ec6fbff58dffefaa108d30164581a30
0-x-14 <0x14@daum.net>
2024-05-10 02:44:41 +0900

    [Docs] 웹서버 이름 및 아이콘 변경

--- src/main/java/ddareunging/ddareunging_server/controller/ApiTestController.java
+++ src/main/java/ddareunging/ddareunging_server/controller/ApiTestController.java
@@ -1,9 +1,11 @@
 package ddareunging.ddareunging_server.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 public class ApiTestController {
     @RequestMapping(value = "/**/{path:[^.]*}")
