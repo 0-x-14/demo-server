35454088bd103e04441e32b12de39ba9b718352f
0-x-14 <0x14@daum.net>
2024-06-09 16:46:57 +0900

    [Feat] 내가 작성한 댓글 조회 API 구현

--- src/main/java/ddareunging/ddareunging_server/controller/MyPageController.java
+++ src/main/java/ddareunging/ddareunging_server/controller/MyPageController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,4 +26,17 @@ public class MyPageController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("공지사항 조회 중 오류가 발생했습니다: " + e.getMessage());
         }
     }
+    // 공지사항 조회
+
+    @GetMapping("/myReply")
+    public ResponseEntity<?> getRepliesByUserId(@RequestParam("user-id") Long userId) {
+        try {
+            return ResponseEntity.ok(myPageService.getRepliesByUserId(userId));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("잘못된 요청입니다: " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("내가 작성한 댓글 조회 중 오류가 발생했습니다: " + e.getMessage());
+        }
+    }
+    // 내가 작성한 댓글 조회
 }
---
+++ src/main/java/ddareunging/ddareunging_server/dto/ReplyDTO.java
@@ -0,0 +1,4 @@
+package ddareunging.ddareunging_server.dto;
+
+public record ReplyDTO(Long replyId, String courseName, String replyContent) {
+}
